ab7cfa893468248d7ca428221f2b32b93cdff6ec
Exequetor <goliathxd@gmail.com>
2018-02-11 00:58:13 -0600

    MySQL script update

--- src/main/resources/DBScript/tutorias.sql
+++ src/main/resources/DBScript/tutorias.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1:3306
--- Tiempo de generación: 08-02-2018 a las 17:57:49
+-- Tiempo de generación: 11-02-2018 a las 06:57:15
 -- Versión del servidor: 5.7.19
 -- Versión de PHP: 5.6.31
 
@@ -21,6 +21,7 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `tutorias`
 --
+
 -- --------------------------------------------------------
 
 --
@@ -37,17 +38,17 @@ CREATE TABLE IF NOT EXISTS `alumno` (
   `correo` tinytext COLLATE utf8_spanish_ci NOT NULL,
   `tutor_id` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 --
 -- Volcado de datos para la tabla `alumno`
 --
 
 INSERT INTO `alumno` (`id`, `matricula`, `nombre`, `apellidos`, `foto`, `correo`, `tutor_id`) VALUES
-(1, '2013020196', 'Gustavo', 'Blizzardi', '2013020196Gustavo.jpg', 'tavo.destructo@hotmail', 0),
-(3, '2013020197', 'Carlos', 'Hernandez', '2013020197Carlos.jpg', 'goliath@gmail.com', 0),
-(6, '2013020200', 'Adrian', 'Gonzales', '2013020200Adrian.jpg', 'ad.gon@gmail.com', 0),
-(7, '2013020201', 'Fernando', 'Garcia  Velasco', '2013020201Fernando.jpg', 'fer.vel@hotmail.com', 0);
+(1, '2013020196', 'Gustavo', 'Blizzardi', '2013020196Gustavo.jpg', 'tavo.destructo@hotmail', 4),
+(3, '2013020197', 'Carlos', 'Hernandez', '2013020197Carlos.jpg', 'goliath@gmail.com', 1),
+(6, '2013020200', 'Adrian', 'Gonzalesz', '2013020200Adrian.jpg', 'ad.gon@gmail.com', 1),
+(8, '2013020202', 'Mariela', 'Miguel', '2013020202Mariela.jpg', 'nicfk@gmail.com', 5);
 
 -- --------------------------------------------------------
 
@@ -76,6 +77,24 @@ CREATE TABLE IF NOT EXISTS `calificaciones` (
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `entrevista`
+--
+
+DROP TABLE IF EXISTS `entrevista`;
+CREATE TABLE IF NOT EXISTS `entrevista` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_alumno` int(11) NOT NULL,
+  `matricula` tinytext COLLATE utf8_spanish_ci NOT NULL,
+  `alumno` tinytext COLLATE utf8_spanish_ci NOT NULL,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `entrevistador` tinytext COLLATE utf8_spanish_ci NOT NULL,
+  `notas` text COLLATE utf8_spanish_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `materia`
 --
 
@@ -84,7 +103,7 @@ CREATE TABLE IF NOT EXISTS `materia` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `nombre` tinytext COLLATE utf8_spanish_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 --
 -- Volcado de datos para la tabla `materia`
@@ -92,7 +111,8 @@ CREATE TABLE IF NOT EXISTS `materia` (
 
 INSERT INTO `materia` (`id`, `nombre`) VALUES
 (2, 'Programacion Estructurada'),
-(3, 'Calculo Integral');
+(3, 'Calculo Integral'),
+(4, 'Quimica');
 
 -- --------------------------------------------------------
 
@@ -108,15 +128,17 @@ CREATE TABLE IF NOT EXISTS `profesor` (
   `correo` tinytext COLLATE utf8_spanish_ci NOT NULL,
   `instituto` tinytext COLLATE utf8_spanish_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 --
 -- Volcado de datos para la tabla `profesor`
 --
 
 INSERT INTO `profesor` (`id`, `nombre`, `usuario`, `correo`, `instituto`) VALUES
-(1, 'Carlos', 'Exe1', 'goliath@gmail', 'Computacion'),
-(3, 'Andrea Martinez Hernandez', 'andy', 'and.her@gmail.com', 'Fisica y Matematicas');
+(1, 'Jose Angel Lopez Bautista', 'Exe1', 'goliath@gmail', 'Computacion'),
+(3, 'Andrea Martinez Hernandez', 'andy', 'and.her@gmail.com', 'Fisica y Matematicas'),
+(4, 'Juan Juarez Fuentes', 'jjf', 'jjf@mixteco.mx', 'Computacion'),
+(5, 'Raul', 'raul', 'raul@mixteco.mx', 'Computacion');
 
 --
 -- Restricciones para tablas volcadas
